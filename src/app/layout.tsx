300922eef816df969ebc365478b88aeff1848d2c
jonathanBensadoun <jonathan.ben-sadoun@oclock.school>
2024-10-03 18:36:38 +0200

    change metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,8 @@ import Navbar from "@/component/Navbar/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Tool for dev",
+  title:
+    "Tool for dev - application outil pour les développeurs generer des qr code avec image ,des dégradés de couleur en css, des mots de passes sécurisés et personalisés , des Dates en JS , redimensionner des image pour faire des favicon, et la possibilité de lire des md ",
   description:
     "application outil pour les développeurs generer des qr code avec image ,des dégradés de couleur en css, des mots de passes sécurisés et personalisés , des Dates en JS , redimensionner des image pour faire des favicon, et la possibilité de lire des md ",
 };
